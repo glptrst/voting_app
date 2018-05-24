53a81702a254b1b62a15d567a1684a07db60086c
G P <giulio.pietroiusti@gmail.com>
2018-05-25 00:27:32 +0200

    Connect to db.

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 "use strict";
 const express = require('express');
+const mongodb = require('mongodb');
 const config = require('./config');
 
+var MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+MongoClient.connect(process.env.DBURI)
+    .then(function(db){
+	console.log('connected to db');
+    }, function(err){
+	console.log(err);
+    }
+	 );
 
 app.get('/', (req, res, next) => {
     res.render('index', {title: 'Voting App'});
