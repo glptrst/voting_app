ac9511b846a7bdf2b930e5e76c25b1bc4d553704
G P <giulio.pietroiusti@gmail.com>
2018-05-26 14:17:22 +0200

    Write async function to connect to db.

--- server.js
+++ server.js
@@ -2,20 +2,13 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const config = require('./config');
+const voting_app = require('./voting_app');
 
 var MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
-
-MongoClient.connect(process.env.DBURI)
-    .then(function(db){
-	console.log('connected to db');
-    }, function(err){
-	console.log(err);
-    }
-	 );
 
 app.get('/', (req, res, next) => {
     res.render('index', {title: 'Voting App'});
