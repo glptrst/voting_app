83311a0d61423be1098fb4490a1bb2959f8f52c0
G P <giulio.pietroiusti@gmail.com>
2018-05-30 14:51:01 +0200

    Pug: extends layout.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET /
 router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+  return res.render('index', { title: 'Voting App' });
 });
 
 module.exports = router;
