c612ec63b28b30b2ee52766f53b8863934f0f78f
G P <giulio.pietroiusti@gmail.com>
2018-06-02 12:36:09 +0200

    Serve sign up page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 // GET /
 router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Voting App' });
+    return res.render('index', { title: 'Voting App' });
+});
+
+
+// GET /register
+router.get('/register', function(req, res, next) {
+    return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /register
+router.post('/register', function(req, res, next) {
+    return res.send('User Created');
 });
 
 module.exports = router;
