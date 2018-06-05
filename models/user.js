05504ce31c81e8d76ac3963db5cee4404345d687
G P <giulio.pietroiusti@gmail.com>
2018-06-05 16:00:18 +0200

    Encrypt user password before storing it into the db.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
-var mongoose = require('mongoose');
-var UserSchema = new mongoose.Schema({
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const UserSchema = new mongoose.Schema({
     email: {
 	type: String,
 	unique: true,
@@ -16,6 +17,17 @@ var UserSchema = new mongoose.Schema({
 	type: String,
 	required: true
     }
+});
+// hash password before saving to database
+UserSchema.pre('save', function(next) {
+    var user = this;
+    bcrypt.hash(user.password, 10, function(err, hash) {
+	if (err) {
+	    return next(err);
+	}
+	user.password = hash;
+	next();
+    });
 });
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
