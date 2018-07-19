f41511eff4abf2c727371f3023c455a81d229f86
G P <giulio.pietroiusti@gmail.com>
2018-07-19 15:50:52 +0200

    Hash and salt user password.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
     email: {
 	type: String,
@@ -17,4 +18,17 @@ var UserSchema = new mongoose.Schema({
     }
 });
 var User = mongoose.model('User', UserSchema);
+// hash password before saving it to database using `pre` method
+UserSchema.pre('save', function(next){
+    // `this`, in this context, refers to the object we created containing the
+    // information the user entered in the sign up form
+    let user = this;
+    bcrypt.hash(user.password, 10, function(err, hash){
+	if (err) {
+	    return next(err);
+	}
+	user.password = hash;
+	next();
+    });
+});
 module.exports = User;
