eba5b3eec71ad815e913486fc421a9be0d98320e
gp <giulio.pietroiusti@gmail.com>
2018-07-29 01:10:05 +0200

    Create `requiresLogin` middleware.

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,18 @@ function loggedOut(req, res, next) {
     if (req.session && req.session.userId) {
 	return res.redirect('/profile');
     }
-    next();
+    return next();
 }
+
+function requiresLogin(req, res, next) {
+    if (req.session && req.session.userId) {
+	return next(); 
+    } else {
+	let err = new Error('You are not logged in');
+	err.status = 401;
+	return next(err);
+    }
+}
+
 module.exports.loggedOut = loggedOut;
+module.exports.requiresLogin = requiresLogin;
