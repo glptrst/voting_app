0858fba32939d6c4bb6e00e2d6f854de4f7b2246
gp <giulio.pietroiusti@gmail.com>
2018-08-09 01:22:06 +0200

    Create Poll model.

---
+++ models/poll.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var PollSchema = new mongoose.Schema({
+    title: {
+	type: String,
+	unique: true,
+	required: true
+    },
+    author: {
+	type: String
+    },
+    options: [{
+	title: {
+	    type: String,
+	    unique: true,
+	    required: true
+	},
+	votes: {
+	    type: Number
+	}
+    }]
+});
+
+var Poll = mongoose.model('Poll', PollSchema);
+
+module.exports = Poll;
